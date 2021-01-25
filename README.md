72590e329df5f7c10a52a2059252a7a55eab40fb
dev-parvej <parvej.code@gmail.com>
2021-01-26 01:53:00 +0600

    Basic graphQL added

--- README.md
+++
@@ -1,1 +0,0 @@
-# express-grphql-practice
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./Schema/Schema')
+const app = express();
+
+app.use(express.json());
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`App is listening to: http://localhost:${PORT}`);
+});
